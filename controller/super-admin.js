e4d6727f0642b3a51d0c38458802bd95c413acf9
sandeep-sana <sandeep.s@clay.co.in>
2025-09-08 16:25:38 +0530

    toogle

--- controller/super-admin.js
+++ controller/super-admin.js
@@ -1,5 +1,6 @@
 const Dao = require('../dao/super-admin');
-const { STATUS } = require('../utils/status.utils');
+const { MESSAGE } = require('../utils/message');
+const { STATUS } = require('../utils/status');
 
 
 const saveAccount = async (req, res) => {
@@ -8,11 +9,15 @@ const saveAccount = async (req, res) => {
     query = query ? JSON.parse(query) : null;
     projection = projection ? JSON.parse(projection) : null;
     options = options ? JSON.parse(options) : null;
+    const isEmailExist = await Dao.findOne({ 'company.email': query.company.email });
+    if (isEmailExist) {
+      return res.status(STATUS.INTERNAL_SERVER_ERROR).json({ message: MESSAGE.EMAIL_ALREADY_EXIST });
+    }
     const account = await Dao.insertOne(query, projection, options);
-    res.status(STATUS.OK).json({ account });
+    return res.status(STATUS.CREATED).json({ account, message: MESSAGE.ACCOUNT_UPDATED_SUCCESSFULLY });
   } catch (error) {
     console.error(error);
-    res.status(STATUS.INTERNAL_SERVER_ERROR).json({ message: 'Server error' });
+    res.status(STATUS.INTERNAL_SERVER_ERROR).json({ message: MESSAGE.SERVER_ERROR });
   }
 }
 const fetchAccount = async (req, res) => {
@@ -22,11 +27,13 @@ const fetchAccount = async (req, res) => {
     projection = projection ? JSON.parse(projection) : null;
     options = options ? JSON.parse(options) : null;
     const account = await Dao.findOne(query, projection, options);
-    console.log(account)
-    res.status(STATUS.OK).json({ account });
+    if (!account) {
+      return res.status(STATUS.INTERNAL_SERVER_ERROR).json({ message: MESSAGE.EMAIL_PASSWORD_NOT_CORRECT });
+    }
+    return res.status(STATUS.OK).json({ account, message: MESSAGE.LOGIN_SUCCESSFULLY });
   } catch (error) {
     console.error(error);
-    res.status(STATUS.INTERNAL_SERVER_ERROR).json({ message: 'Server error' });
+    res.status(STATUS.INTERNAL_SERVER_ERROR).json({ message: MESSAGE.SERVER_ERROR });
   }
 }
 
---
+++ utils/message.js
@@ -0,0 +1,21 @@
+
+const COMMON = {
+    UPDATED_SUCCESSFULLY: 'updated successfully!',
+    CREATED_SUCCESSFULLY: 'created successfully!',
+    IS_REQUIRED: 'is required',
+    NOT_FOUND: 'not found',
+}
+
+const MESSAGE = {
+    SERVER_ERROR: 'Server error',
+    LOGIN_SUCCESSFULLY: 'Login successfully',
+    EMAIL_ALREADY_EXIST: 'Email already exist',
+    EMAIL_PASSWORD_NOT_CORRECT: 'Email password not correct',
+
+    ACCOUNT_UPDATED_SUCCESSFULLY: `Account ${COMMON.UPDATED_SUCCESSFULLY}`,
+
+}
+
+module.exports = {
+    MESSAGE
+};
