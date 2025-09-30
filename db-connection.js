5ad237e63ab319876928a4f19163c38933b1f7be
sandeep-sana <sandeep.s@clay.co.in>
2025-09-30 16:06:33 +0530

    prod

--- db-connection.js
+++ db-connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbconnection = async (dbName="toggle", collectionName, Schema) => {
-    const dbURI = `${process.env.DATABASE_URI}${dbName}`;
+    const dbURI = `${process.env.DATABASE_URI}${dbName}?authSource=admin`;
     const connection = await mongoose.createConnection(dbURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
